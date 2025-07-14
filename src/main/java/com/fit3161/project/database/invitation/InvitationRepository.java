34d56b71ce883b43dc361719cfa451bfefc8d608
mishfishdish <mishfishhh@gmail.com>
2025-07-14 22:30:59 +1000

    [devops] fix schema

--- src/main/java/com/fit3161/project/database/invitation/InvitationRepository.java
+++
@@ -1,4 +0,0 @@
-package com.fit3161.project.database.invitation;
-
-public class InvitationRepository {
-}
--- src/main/java/com/fit3161/project/database/invitation/enums/Role.java
+++
@@ -1,6 +0,0 @@
-package com.fit3161.project.database.invitation.enums;
-
-public enum Role {
-    ADMIN,
-    MEMBER
-}
