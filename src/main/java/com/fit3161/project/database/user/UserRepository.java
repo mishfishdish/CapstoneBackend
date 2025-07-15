ce68b0ce2d8a0bb76663738a6f727352d3d2a084
mishfishdish <mishfishhh@gmail.com>
2025-07-15 21:42:54 +1000

    [onboarding] fix query

--- src/main/java/com/fit3161/project/database/user/UserRepository.java
+++ src/main/java/com/fit3161/project/database/user/UserRepository.java
@@ -1,14 +1,22 @@
 package com.fit3161.project.database.user;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends CrudRepository<UserRecord, UUID> {
 
     boolean existsByEmailAndPasswordHash(String email, String password);
     boolean existsByEmail(String email);
-    UserRecord findUserRecordIdByEmailOrUserId(String email);
+
+    @Query("""
+    SELECT u FROM UserRecord u 
+    WHERE u.email = :value OR CAST(u.userId AS string) = :value
+    """)
+    Optional<UserRecord> findUserRecordIdByEmailOrUserId(@Param("value") String value);
 
 }
 
