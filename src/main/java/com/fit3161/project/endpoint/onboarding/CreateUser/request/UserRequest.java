834b7b687dffbd140fc52a98632c3831bdb69d47
mishfishdish <mishfishhh@gmail.com>
2025-08-16 16:43:37 +1000

    [swagger] update onboarding apis

--- src/main/java/com/fit3161/project/endpoint/onboarding/CreateUser/request/UserRequest.java
+++ src/main/java/com/fit3161/project/endpoint/onboarding/CreateUser/request/UserRequest.java
@@ -1,13 +1,10 @@
 package com.fit3161.project.endpoint.onboarding.CreateUser.request;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class UserRequest {
 
-    private String username;
     private String firstName;
     private String lastName;
     private String email;
