805c7df859c21552a1dcc900aeb4d0a306ea2e15
mishfishdish <mishfishhh@gmail.com>
2025-09-21 20:54:08 +1000

    [postman] weoioiedw

--- src/main/java/com/fit3161/project/endpoint/general/getActivities/ActivityResponse.java
+++ src/main/java/com/fit3161/project/endpoint/general/getActivities/ActivityResponse.java
@@ -1,19 +1,29 @@
 package com.fit3161.project.endpoint.general.getActivities;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 
-@AllArgsConstructor
 @Data
 public class ActivityResponse {
-    UUID activityId;
+    String activityId;
     String activityTitle;
     LocalDateTime startTime;
     LocalDateTime endTime;
     String type;
     UUID dependsOnEventId;
+
+    public ActivityResponse(String activityId, String activityTitle,
+                            Timestamp startTime, Timestamp endTime,
+                            String type, UUID dependsOnEventId) {
+        this.activityId = activityId;
+        this.activityTitle = activityTitle;
+        this.startTime = startTime != null ? startTime.toLocalDateTime() : null;
+        this.endTime = endTime != null ? endTime.toLocalDateTime() : null;
+        this.type = type;
+        this.dependsOnEventId = dependsOnEventId;
+    }
 }
